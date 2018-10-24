b8a8f99529fcd9bf5e39f6a570dbb71ba829d844
QuinJetZhou <zwq0807@gmail.com>
2018-10-24 20:55:02 +0800

    study now
    get up!

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: ZHOU
+* @Date:   2018-09-28 18:44:28
+* @Last Modified by:   ZHOU
+* @Last Modified time: 2018-09-30 16:40:35
+*/
+// var app = new Vue({
+// 	el:"#app",
+// 	data:{
+// 		message:'Hello Vue!'
+// 	}
+// })
